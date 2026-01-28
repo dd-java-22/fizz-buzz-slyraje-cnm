081b97035b4ed1be2360daa2c62c9e08275821dc
Michael Sylvester <msylvester@cnm.edu>
2026-01-28 16:50:23 -0700

    refactored Main by replacing conditional output logic with Map lookup via getOrDefault

--- src/main/java/edu/cnm/deepdive/fizzbuzz/Main.java
+++ src/main/java/edu/cnm/deepdive/fizzbuzz/Main.java
@@ -1,8 +1,18 @@
 package edu.cnm.deepdive.fizzbuzz;
 
+import static java.util.Map.entry;
+
+import java.util.EnumSet;
+import java.util.Map;
 import java.util.Set;
 
 public class Main {
+
+  private static final Map<Set<FizzBuzz>, String> FIZZBUZZ_MAP = Map.ofEntries(
+      entry(EnumSet.of(FizzBuzz.FIZZ), "Fizz"),
+      entry(EnumSet.of(FizzBuzz.BUZZ), "Buzz"),
+      entry(EnumSet.of(FizzBuzz.FIZZ, FizzBuzz.BUZZ), "FizzBuzz")
+      );
 
   static void main(String[] args) {
     int max = 100;
@@ -12,16 +22,8 @@ public class Main {
     }
     for (int i = 1; i < max; i++) {
       Set<FizzBuzz> result = FizzBuzz.fizzBuzz(i);
-      if (result.contains(FizzBuzz.FIZZ)) {
-        System.out.print("Fizz");
-      }
-      if (result.contains(FizzBuzz.BUZZ)) {
-        System.out.print("Buzz");
-      }
-      if (result.isEmpty()) {
-        System.out.print(i);
-      }
-      System.out.println();
+      System.out.println(FIZZBUZZ_MAP.getOrDefault(result,Integer.toString(i)));
+
     }
   }
 
